77fc0c54e673b79234f120a4881a624e1f2cf7b1
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-02-18 00:09:25 +0200

    addRegisterComponentToTestCustomAndMultiValidators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from 'src/Components/default/default.component';
+import { RegisterComponent } from 'src/Components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/Default',
+    pathMatch: 'full'
+  },
+  {
+    path: 'Default',
+    component: DefaultComponent
+  },
+  { path: 'Register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { DefaultComponent } from 'src/Components/default/default.component';
+import { RegisterComponent } from 'src/Components/register/register.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  exports: [
+    //CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RegisterComponent,
+    DefaultComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
