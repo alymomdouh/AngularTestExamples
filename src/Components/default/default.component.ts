b474b3e6bcc9800b4e75bdb1d8858d3cbfc06c9f
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-02-18 00:01:45 +0200

    add default

---
+++ src/Components/default/default.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.css']
+})
+export class DefaultComponent implements OnInit {
+
+  title = 'PassValidationExample';
+  constructor(private router: Router) { }
+  btnRegisterClick = () => {
+    this.router.navigateByUrl('/Register');
+  };
+  ngOnInit(): void {
+  }
+}
